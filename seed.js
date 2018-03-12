250f006952ad331906cb64cafedf7b127c90ee73
Kẏra <kxra@riseup.net>
2018-03-12 17:32:16 -0400

    woohoo, seeded!

--- seed.js
+++ seed.js
@@ -4,12 +4,10 @@ const newGardener1 = new Gardener({
   name: "Billy Joe",
   age: 999
 });
-
 const newGardener2 = new Gardener({
   name: "Jane Doe",
   age: 50
 });
-
 const newGardener3 = new Gardener({
   name: "John Smith",
   age: 75
@@ -18,16 +16,17 @@ const newGardener3 = new Gardener({
 const newPlot1 = new Plot({
   size: 10,
   shaded: true
+  // gardenerId: newGardener1.id
 });
-
 const newPlot2 = new Plot({
   size: 20,
   shaded: false
+  // gardenerId: newGardener2.id
 });
-
 const newPlot3 = new Plot({
   size: 50,
   shaded: true
+  // gardenerId: newGardener3.id
 });
 
 const newVegetable1 = new Vegetable({
@@ -35,28 +34,42 @@ const newVegetable1 = new Vegetable({
   color: "Orange",
   planted_on: "2012-01-12"
 });
-
 const newVegetable2 = new Vegetable({
   name: "Tomato",
   color: "Red",
   planted_on: "2014-05-24"
 });
-
 const newVegetable3 = new Vegetable({
   name: "Lettuce",
   color: "Green",
   planted_on: "2016-06-06"
 });
 
-
 db
   .sync({ force: true })
   .then(() => {
-    return Promise.all([newVegetable1.save(), newVegetable2.save(), newVegetable3.save()])
+    return Promise.all([
+      newVegetable1.save(),
+      newVegetable2.save(),
+      newVegetable3.save()
+    ]);
   })
-  // .then(() => {
-  //   return newVegetable3.save();
-  // })
+  .then(() => {
+    newGardener1.favoriteVegetableId = newVegetable1.id;
+    newGardener2.favoriteVegetableId = newVegetable2.id;
+    newGardener3.favoriteVegetableId = newVegetable3.id;
+    return Promise.all([
+      newGardener1.save(),
+      newGardener2.save(),
+      newGardener3.save()
+    ]);
+  })
+  .then(() => {
+    newPlot1.gardenerId = newGardener1.id;
+    newPlot2.gardenerId = newGardener2.id;
+    newPlot3.gardenerId = newGardener3.id;
+    return Promise.all([newPlot1.save(), newPlot2.save(), newPlot3.save()]);
+  })
   .then(() => {
     console.log("Database synced!");
     db.close();
