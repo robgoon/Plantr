7e83f8b17adda3905c2fb242eb4de927969142f8
Robert Goon <rob.goon@gmail.com>
2018-03-12 16:34:53 -0400

    v1

---
+++ models.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/plantr', {logging: false});
+
+const Gardener = db.define('gardener', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+})
+
+const Plot = db.define('plot', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+})
+
+const Vegetable = db.define('vegetable', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planed_on: Sequelize.DATE
+})
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
+
+module.exports = { db }
